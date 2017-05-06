319afddb1e603a39b36a9b518ec2e0e74386de77
Maxwell Friederichs <orngcrsh2010@gmail.com>
2017-05-06 09:51:24 -0500

    Initial import of moving cube

---
+++ graphics.c
@@ -0,0 +1,87 @@
+#include <SDL.h>
+#include <GL/gl.h>
+
+
+int main() {
+   SDL_Window *window;
+   SDL_Init(SDL_INIT_VIDEO|SDL_INIT_AUDIO);
+
+   window = SDL_CreateWindow(
+      "An SDL2 window",                  // window title
+      SDL_WINDOWPOS_UNDEFINED,           // initial x position
+      SDL_WINDOWPOS_UNDEFINED,           // initial y position
+      640,                               // width, in pixels
+      480,                               // height, in pixels
+      SDL_WINDOW_OPENGL                  // flags - see below
+   );
+
+   SDL_GLContext context = SDL_GL_CreateContext(window);
+   SDL_GL_MakeCurrent(window, context);
+
+   glEnable(GL_DEPTH_TEST);
+
+
+   int idk = 0;
+
+   while(1){
+      SDL_PumpEvents();
+      glRotatef(1,5,5,5);
+      glBegin(GL_QUADS);
+
+      glColor3ub(idk++ & 0x0FF, 0x86, 0xf4);
+
+      // Front face
+      glVertex3f(0.5,0.5,0.5);
+      glVertex3f(0.5,-0.5,0.5);
+      glVertex3f(-0.5,-0.5,0.5);
+      glVertex3f(-0.5,0.5,0.5);
+
+
+      glColor3ub(0xb9, 0xf4, 0x42);
+      // Back face
+      glVertex3f(0.5,0.5,-0.5);
+      glVertex3f(0.5,-0.5,-0.5);
+      glVertex3f(-0.5,-0.5,-0.5);
+      glVertex3f(-0.5,0.5,-0.5);
+
+
+      glColor3ub(0xef, 0x04, 0x10);
+      // Top face
+      glVertex3f(0.5,0.5,0.5);
+      glVertex3f(0.5,0.5,-0.5);
+      glVertex3f(-0.5,0.5,-0.5);
+      glVertex3f(-0.5,0.5,0.5);
+
+
+      glColor3ub(0xf1, 0xf4, 0x42);
+      // Bottom face
+      glVertex3f(0.5,-0.5,0.5);
+      glVertex3f(0.5,-0.5,-0.5);
+      glVertex3f(-0.5,-0.5,-0.5);
+      glVertex3f(-0.5,-0.5,0.5);
+
+      glColor3ub(0xf4, 0x42, 0xce);
+      // right face
+      glVertex3f(0.5,-0.5,0.5);
+      glVertex3f(0.5,-0.5,-0.5);
+      glVertex3f(0.5,0.5,-0.5);
+      glVertex3f(0.5,0.5,0.5);
+
+      glColor3ub(0xf4, 0x42, 0xce);
+      // left face
+      glVertex3f(-0.5,-0.5,0.5);
+      glVertex3f(-0.5,-0.5,-0.5);
+      glVertex3f(-0.5,0.5,-0.5);
+      glVertex3f(-0.5,0.5,0.5);
+
+      glEnd();
+      SDL_GL_SwapWindow(window);
+      glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+   }
+
+   SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
+   SDL_DestroyWindow(window);
+
+   SDL_Quit();
+   return 0;
+}
